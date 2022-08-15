15587b192d03ed04517e5db6584fc97e8449bdfa
Rajin, MD. Iktedar Hasan Rushdi <rushdi2410@gmail.com>
2022-08-15 20:22:23 +0600

    API for blogs updated

---
+++ blog/app/Http/Controllers/userBlogControllerAPI.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Blogs;
+use App\Models\Users;
+use App\Models\Comments;
+use Illuminate\Http\Request;
+
+class userBlogControllerAPI extends Controller
+{
+    public function viewBlog(){
+        $blogs = blogs::all();
+        return $blogs;
+    }
+
+    public function createBlog(Request $request)
+    {
+        $blogs = new blogs();
+        $blogs->title = $request->title;
+        $blogs->slug = $request->slug; 
+        $blogs->description = $request->description;
+        $blogs->image = $request->image;
+        $blogs->userID = $request->userID;
+        $blogs->save();
+
+        return response()->json('Blog Created');
+    }
+}
--- blog/routes/api.php
+++ blog/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\userBlogControllerAPI;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/blogs/viewBlog',[userBlogControllerAPI::class,'viewBlog']);
+Route::post('/createBlog', [userBlogControllerAPI::class, 'createBlog']);
